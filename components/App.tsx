2bf09befabfea15513cb38d6a2f038ddc0700629
Garfewxz <poottapan99@gmail.com>
2025-08-29 13:04:00 +0700

    s

---
+++ components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SubjectGradeManager from './components/SubjectGradeManager';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>🎓 ระบบจัดการรายวิชา</h1>
+      <SubjectGradeManager />
+    </div>
+  );
+};
+
+export default App;
